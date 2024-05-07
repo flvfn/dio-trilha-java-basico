c85d48392440863c60bbb7cd074882cbc9e9a73b
Flavio Nascimento <flaviofan@live.com>
2024-05-07 10:04:20 -0300

    Implementação dos passos propostos

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //Exibir mensagens para o usuário
+        //Obter pela scanner os valores digitados no terminal
+
+        System.out.println("Por favor digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite sua Agência: ");
+        String numeroAgencia = scanner.next();
+     
+        System.out.println("Por favor, insira seu Nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, insira seu Sobrenome: ");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Por favor, insira seu saldo");
+        double saldoConta = scanner.nextDouble();
+
+        
+        //Exibir mensagem conta criada
+        System.out.println("Olá " + nomeCliente.concat(" ").concat(sobrenomeCliente)+ ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + 
+                            ", conta " + numeroConta + " e seu saldo R$ " + saldoConta + " já está disponível para saque.");
+
     }
 }
